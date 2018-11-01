79420d20532bbcda08dfa576e1ceaba0ebb8ad93
baituo <baituo@gaoding.com>
2018-11-01 14:27:03 +0800

    chore: 修改eslint，添加vue-markdown-loader

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,25 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
+	root: true,
+	env: {
+		node: true
+	},
+	'extends': [
+		'plugin:vue/essential',
+		'eslint:recommended'
+	],
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'indent': ['error', 'tab'], // tab缩进
+		'linebreak-style': ['error', 'unix'],
+		'quotes': ['error', 'single'], // 字符串单引号
+		'semi': ['error', 'always'], // 必须分号结尾
+		'curly': 1, // 必须使用if() {} 的‘{}’
+		'no-multiple-empty-lines': [2, {'max': 3}], //空行最多不能超过3行
+		'space-before-function-paren': [2, 'never'], // 函数定义括号前的空格
+		'comma-dangle': [2, 'never'], // 对象字面量项尾不能有逗号
+	},
+	parserOptions: {
+		parser: 'babel-eslint'
+	}
 }
--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,38 @@
 module.exports = {
-  // 修改 src 为 examples
-  pages: {
-    index: {
-      entry: 'examples/main.js',
-      template: 'public/index.html',
-      filename: 'index.html'
-    }
-  },
-  // 强制内联CSS
-  css: { extract: false },
-  // 扩展 webpack 配置，使 packages 加入编译
-  chainWebpack: config => {
-    config.module
-      .rule('js')
-      .include
-      .add('/packages')
-      .end()
-      .use('babel')
-      .loader('babel-loader')
-      .tap(options => {
-        // 修改它的选项...
-        return options
-      })
-  }
-}
+// 修改 src 为 examples
+	pages: {
+		index: {
+			entry: 'examples/main.js',
+			template: 'public/index.html',
+			filename: 'index.html'
+		}
+	},
+	// 强制内联CSS
+	css: {
+		extract: false
+	},
+	// 扩展 webpack 配置，使 packages 加入编译
+	chainWebpack: config => {
+		config.module
+			.rule('js')
+			.include
+			.add('/packages')
+			.end()
+			.use('babel')
+			.loader('babel-loader')
+			.tap(options => {
+				// 修改它的选项...
+				return options;
+			});
+		config.module.rule('md')
+			.test(/\.md/)
+			.use('vue-loader')
+			.loader('vue-loader')
+			.end()
+			.use('vue-markdown-loader')
+			.loader('vue-markdown-loader/lib/markdown-compiler')
+			.options({
+				raw: true
+			});
+	}
+};
